dec4749b385cf5d99dae2798c3cde96823b0dbf1
SolarTrip <55459751+SolarTrip@users.noreply.github.com>
2021-01-08 11:18:58 -0800

    Add files via upload

--- LEDMatrix.py
+++ LEDMatrix.py
@@ -35,7 +35,35 @@ data = [     # data of "0-F"
     0x00, 0x00, 0x7F, 0x48, 0x48, 0x40, 0x00, 0x00, # "F"
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, # " "
 ]
+def setup():
+    GPIO.setmode(GPIO.BOARD)    # use PHYSICAL GPIO Numbering
+    GPIO.setup(dataPin, GPIO.OUT)
+    GPIO.setup(latchPin, GPIO.OUT)
+    GPIO.setup(clockPin, GPIO.OUT)
+    
 
+def loop():
+    while True:
+        for j in range(0,500): # Repeat enough times to display the smiling face a period of time
+            x=0x80
+            for i in range(0,8):
+                GPIO.output(latchPin,GPIO.LOW)
+                shiftOut(dataPin,clockPin,MSBFIRST,pic[i]) #first shift data of line information to first stage 74HC959
+
+                shiftOut(dataPin,clockPin,MSBFIRST,~x) #then shift data of column information to second stage 74HC959
+                GPIO.output(latchPin,GPIO.HIGH) # Output data of two stage 74HC595 at the same time
+                time.sleep(0.001) # display the next column
+                x>>=1
+        for k in range(0,len(data)-8): #len(data) total number of "0-F" columns 
+            for j in range(0,20): # times of repeated displaying LEDMatrix in every frame, the bigger the "j", the longer the display time.
+                x=0x80      # Set the column information to start from the first column
+                for i in range(k,k+8):
+                    GPIO.output(latchPin,GPIO.LOW)
+                    shiftOut(dataPin,clockPin,MSBFIRST,data[i])
+                    shiftOut(dataPin,clockPin,MSBFIRST,~x)
+                    GPIO.output(latchPin,GPIO.HIGH)
+                    time.sleep(0.001)
+                    x>>=1
 def destroy():  
     GPIO.cleanup()
 if __name__ == '__main__':  # Program entrance
